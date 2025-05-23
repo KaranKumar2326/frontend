e2a75600a99a77b5fa47a6a96fd0cb4bb59da769
Siddharth Rai <siddharth.rai@thoughtworks.com>
2025-05-23 22:09:53 +0530

    done with signup

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -4,6 +4,7 @@ import './HomePage.css';
 import { Music, MapPin, Phone, Mail, Facebook, Instagram, Twitter, Youtube ,Search , Headphones , CalendarCheck} from "lucide-react";
 import "./Footer.css"; 
 import {motion} from "framer-motion";
+import Header from '../Header'; // Import the reusable Header component
 export const Motion = motion;
 
 const HomePage = () => {
@@ -34,26 +35,7 @@ const HomePage = () => {
         {/* <div className='hero-container' style={{ position: 'relative', width: '100%', height: '100vh', overflow: 'hidden' }}> */}
           <div className="background-image">
           </div>
-            <header className="hero-section" style={{ zIndex: 1, height:'100vh', padding: '20px', boxSizing: 'border-box', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>
-              <div className='content'>
-                <div>
-                  <h1 className="hero-title" style={{ color: 'white', fontWeight: 'bold', marginBottom: '20px', fontSize: '3rem' }}>Welcome to Musical Meet</h1>
-                </div>
-                <div>
-                  <p className="hero-subtitle" style={{ color: 'white', fontWeight: 'bold', marginBottom: '30px', fontSize: '1.5rem' }}>
-                  Join us in celebrating the joy of music and connecting with artists from around the world.
-                  </p>
-                </div>
-                <div className="button-container" style={{ display: 'flex', gap: '10px', justifyContent: 'left' }}>
-                  <button className="button" style={{ padding: '20px 40px', fontSize: '1rem', borderRadius: '5px', backgroundColor: '#6c2bd9', color: 'white', border: 'none', cursor: 'pointer', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)' }}>
-                    Hire an Artist
-                  </button>
-                  <button className="button" style={{ padding: '20px 40px', fontSize: '1rem', borderRadius: '5px', backgroundColor: '#6c2bd9', color: 'white', border: 'none', cursor: 'pointer', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)' }}>
-                    Join as a Artist
-                  </button>
-                </div>
-              </div>
-            </header>
+            <Header />
         {/* </div> */}
       </div>
       <section className="content-section" style={{ zIndex: 1, maxWidth: '800px', padding: '20px', boxSizing: 'border-box', marginTop: '40px', alignContent: 'center', textAlign: 'left', backgroundColor: 'rgba(255, 255, 255, 0.8)', borderRadius: '10px', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)' }}>
